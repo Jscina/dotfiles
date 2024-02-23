5b93ef21eb2b1dba5f4843f3fc01ae298304570d
Josh <joshscina@gmail.com>
2024-02-22 21:17:31 -0500

    Correct configuration for nvim-dev-containers

--- .config/nvim/lua/plugins/devcontainer.lua
+++ .config/nvim/lua/plugins/devcontainer.lua
@@ -1,6 +1,23 @@
 return {
   "https://codeberg.org/esensar/nvim-dev-container",
   config = function()
-    require("devcontainer").setup({})
+    require("devcontainer").setup({
+      attach_mounts = {
+        neovim_config = {
+          enabled = true,
+          options = { "readonly" },
+        },
+        neovim_data = {
+          enabled = true,
+          options = {},
+        },
+        neovim_state = {
+          enabled = true,
+          options = {},
+        },
+      },
+      container_runtime = "devcontainer-cli",
+      backup_runtime = "docker",
+    })
   end,
 }
