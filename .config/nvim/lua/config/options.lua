9ae46edd9535a69d78fb78afa47adac22ae76f42
Joshua Scina <joshscina@gmail.com>
2024-05-20 19:14:58 -0400

    Update Neovim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,12 +7,4 @@ vim.opt.foldcolumn = "1"
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = false
-
-if vim.g.neovide then
-  vim.g.neovide_window_blurred = true
-  vim.g.neovide_transparecy = 0.7
-  vim.g.neovide_cursor_animation_length = 0.03
-  vim.g.neovide_cursor_trail_length = 0.2
-  vim.g.neovide_cursor_animate_in_insert_mode = true
-  vim.g.neovide_cursor_animate_command_line = false
-end
+vim.opt.clipboard = "unnamedplus"
