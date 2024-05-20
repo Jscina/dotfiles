239207c48e1c78a66fa4e164c384c049db2a7cf8
Josh <joshscina@gmail.com>
2024-05-20 17:21:52 -0400

    Removed kde desktop. Fixed dolphin being stupid, update neovim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
