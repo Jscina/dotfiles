bf092d031421aad10234956d60f41fe92024628a
Josh <joshscina@gmail.com>
2024-07-11 22:32:33 -0400

    fix: neotest + neotree not setting the cwd

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,9 +1,12 @@
 return {
-  {
-    "marilari88/neotest-vitest",
-  },
+  { "marilari88/neotest-vitest" },
   {
     "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-vitest", "rustaceanvim", "neotest-python" } },
+    depenedencies = { "mrcjkb/rustaceanvim", "nvim-neotest/neotest-python", "marilari88/neotest-vitest" },
+    opts = { adapters = {
+      ["rustaceanvim.neotest"] = {},
+      "neotest-python",
+      "neotest-vitest",
+    } },
   },
 }
---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      bind_to_cwd = true,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+    buffers = {
+      follow_current_file = { enabled = true },
+    },
+  },
+}
