baf667a0e190c5499262486aa585397fdd2a8b36
Josh <joshscina@gmail.com>
2024-07-31 21:01:34 -0400

    Remove linux specific configs

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ "$EUID" -ne 0 ]; then
 	echo "Please run as root"
@@ -6,9 +6,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo <<EOF
-This is a post-install script for Arch Linux. It sets up the system for my personal use. 
-DO NOT RUN THIS SCRIPT IF YOU DO NOT KNOW WHAT IT DOES.
-OR IF YOUR SYSTEM IS NOT A FRESH INSTALL.
+This is a setup script for Mac OS X.
 EOF
 
 $confirm = read -p "Do you want to continue? [y/N]" -n 1 -r
@@ -16,98 +14,3 @@ $confirm = read -p "Do you want to continue? [y/N]" -n 1 -r
 if [[ ! $confirm =~ ^[Yy]$ ]]; then
 	exit
 fi
-
-echo "Install yay"
-pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-rm -rf yay
-
-echo "Installing fonts"
-yay -S --noconfirm ttf-ms-win11-auto otf-san-francisco oft-san-francisco-mono ttf-jetbrains-mono-nerd
-
-echo "Installing Dependencies"
-pacman -S --noconfirm git zsh stow zoxide tmux sddm pacman-contrib
-
-echo "Installing ohmyzsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Installing zsh plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "Setup Hyprland"
-yay -S --noconfirm anyrun pam_autologin firefox-nightly-bin nwg-look \
-	hyprshot-git hyprland-git hyprpaper-git hyprlock-git hypridle-git alacritty swaync \
-	xdg-desktop-portal-hyprland-git polkit-kde-agent qt6-wayland \
-	plasma-framework5 waybar qt6ct kvantum gnome-keyring brightnessctl libreoffice-fresh
-
-echo "Adding required pam modules"
-cat <<EOF >>/etc/pam.d/login
-auth required pam_autologin.so always
-auth optional pam_gnome_keyring.so
-session optional pam_gnome_keyring.so auto_start
-EOF
-
-echo "Fixing hyprlock module"
-rm /etc/pam.d/hyprlock
-cat <<EOF >>/etc/pam.d/hyprlock
-auth include system-auth
-EOF
-
-echo "Linking dotfiles"
-stow .
-
-echo "Installing Docker"
-pacman -S --noconfirm docker docker-buildx
-systemctl enable docker
-systemctl start docker
-
-echo "Adding user to docker group"
-usermod -aG docker $USER
-
-echo "Setup Tmux"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-echo "Setup Neovim"
-pacman -S --noconfirm neovim lazygit ripgrep fzf fd lazydocker
-
-echo "Installing Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-echo "Installing Javascript"
-sudo pacman -S nodejs npm
-
-echo "Attempting to rice Firefox"
-curl -s -o- https://raw.githubusercontent.com/PROxZIMA/Sweet-Pop/master/programs/install-curl.sh | bash
-
-$confirm = read -p "Do you want to install nvidia drivers? [y/N]" -n 1 -r
-
-if [[ $confirm =~ ^[Yy]$ ]]; then
-	echo "Installing Nvidia Drivers"
-	pacman -S --noconfirm nvidia nvidia-utils nvidia-settings lib32-nvidia-utils egl-wayland libva-nvidia-driver
-	echo "Adding nvidia modules to mkinitcpio"
-	sed -i 's/^MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-	echo "Adding systemd to mkinitcpio"
-	yay -S --noconfirm nvidia-pacman-hook
-	echo "Create nvidia config"
-	echo "options nvidia_drm modeset=1 fbdev=1" >/etc/modprobe.d/nvidia.conf
-
-	echo "Rebuilding initramfs"
-	mkinitcpio -P
-
-	echo "Enabling nvidia services"
-	systemctl enable nvidia-suspend
-	systemctl enable nvidia-resume
-	systemctl enable nvidia-hibernate
-fi
-
-$confirm = read -p "Do you want to install Steam? [y/N]" -n 1 -r
-
-if [[ $confirm =~ ^[Yy]$ ]]; then
-	echo "Installing Steam"
-	pacman -S --noconfirm steam
-fi
-
-echo "Cleaning up"
-pacman -Rns --noconfirm $(pacman -Qtdq)
-paccache -rk0
