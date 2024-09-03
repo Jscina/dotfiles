c824231a340100d7ce8341865c2e3a6bc7570a08
Joshua Scina <jscina@Joshuas-MacBook-Pro.local>
2024-09-03 08:06:32 -0400

    Add jest to testing utils

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,12 +1,21 @@
 return {
   { "marilari88/neotest-vitest" },
+  { "nvim-neotest/neotest-jest" },
   {
     "nvim-neotest/neotest",
-    depenedencies = { "mrcjkb/rustaceanvim", "nvim-neotest/neotest-python", "marilari88/neotest-vitest" },
-    opts = { adapters = {
-      ["rustaceanvim.neotest"] = {},
-      "neotest-python",
-      "neotest-vitest",
-    } },
+    depenedencies = {
+      "mrcjkb/rustaceanvim",
+      "nvim-neotest/neotest-python",
+      "marilari88/neotest-vitest",
+      "nvim-neotest/neotest-jest",
+    },
+    opts = {
+      adapters = {
+        ["rustaceanvim.neotest"] = {},
+        "neotest-python",
+        "neotest-vitest",
+        "neotest-jest",
+      },
+    },
   },
 }
