926dc662b1fbbf0ff288464f5475fb7992746788
Josh <joshscina@gmail.com>
2024-09-07 15:03:26 -0400

    Add undotree, update neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,5 +11,5 @@ vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
 vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
 vim.keymap.set("n", "<leader>ua", "<cmd>UfoEnable<CR>", { desc = "Enable UFO", noremap = true, silent = true })
 
--- UndoTree Keymaps
+-- Toggle undotree
 vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
