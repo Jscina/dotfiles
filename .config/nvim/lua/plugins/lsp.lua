4dfc1311e0ef3515a5d69c9c94525d581b45e7c5
Jscina <joshscina@gmail.com>
2025-10-26 01:30:59 -0400

    prevent steam from hopping spaces, remove opencode and upgrade neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -17,26 +17,27 @@ vim.g.rustaceanvim = {
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = {
-        enabled = false,
-      },
-      setup = {
-        rust_analyzer = function()
-          return true
-        end,
-      },
-      servers = {
-        tailwindcss = {
-          settings = {
-            experimental = {
-              classRegex = {
-                '"(.*)"',
-              },
+    opts = function(_, opts)
+      opts.inlay_hints = opts.inlay_hints or {}
+      opts.inlay_hints.enabled = false
+
+      opts.setup = opts.setup or {}
+      opts.setup.rust_analyzer = function()
+        return true
+      end
+
+      opts.servers = opts.servers or {}
+
+      opts.servers.tailwindcss = {
+        settings = {
+          experimental = {
+            classRegex = {
+              '"(.*)"',
             },
           },
         },
-      },
-    },
+      }
+      return opts
+    end,
   },
 }
