e419f99c363e41d71803cbae679953790a2ba814
Jscina <joshscina@gmail.com>
2026-08-22 23:39:53 -0400

    fix the windows

--- .config/hypr/userprefs.lua
+++ .config/hypr/userprefs.lua
@@ -4,42 +4,17 @@
 -- Set your personal hyprland configuration here
 -- See https://wiki.hypr.land/Configuring for more information
 
--- // █ █▄░█ █▀█ █░█ ▀█▀
--- // █ █░▀█ █▀▀ █▄█ ░█░
-
--- Uncomment to enable // change to a preferred value
--- 🔗 See https://wiki.hypr.land/Configuring/Basics/Variables/ (input section)
--- All hyprland.conf { category { ... } } blocks become one hl.config()
--- call with matching nested tables.
 hl.config({
-	input = {
-		-- kb_layout = "us",
-		-- follow_mouse = 1,
-		-- sensitivity = 0,
-		-- force_no_accel = false,
-		-- accel_profile = "flat",
-		-- numlock_by_default = true,
-
-		-- 🔗 See https://wiki.hypr.land/Configuring/Basics/Variables/ (touchpad section)
-		touchpad = {
-			natural_scroll = false,
-		},
+	decoration = {
+		rounding = 10,
+		shadow = { enabled = true, range = 20, render_power = 3, color = "rgba(1a1a1aee)" },
+		blur = { enabled = true, size = 3, passes = 3 },
 	},
-
-	-- 🔗 See https://wiki.hypr.land/Configuring/Basics/Variables/ (gestures section)
-	gestures = {
-		-- workspace_swipe = true,
-		-- workspace_swipe_fingers = 3,
-	},
-
-	-- for window swallow, similar to devour
-	misc = {
-		-- enable_swallow = true,
-		-- swallow_regex = "(foot|kitty|allacritty|Alacritty|ghostty|Ghostty|org.wezfurlong.wezterm)",
-	},
-
-	-- Don't show update news on first launch
-	ecosystem = {
-		-- no_update_news = true,
+	general = {
+		gaps_in = 5,
+		gaps_out = 10,
+		border_size = 2,
+		["col.active_border"] = "rgba(cba6f7ee)",
+		["col.inactive_border"] = "rgba(595959aa)",
 	},
 })
